657712ec82948ada9709036649a5074c8bc61496
MichaelD7 <mdevenish@Unknown-3c-15-c2-e8-1e-c6.home>
2019-04-07 16:39:38 +0100

    Move test files to separate directory

---
+++ test_cases/channel_test.py
@@ -0,0 +1,49 @@
+import sys
+from os import path
+sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
+from channel import Channel
+# main
+"""enter flow, width, depth, length, us invert, ds invert, Ks, kinematic viscocity"""
+# channel1 = channel.Channel(0.5, 1.0, 2.0, 100, 0.1, 0.0, 0.003, 1.141e-06, 0.4)
+print("Test Run 1")
+channel1 = Channel()
+channel1.setValues(0.5, 0.8, 2, 100.0, 1.0, 0.9, 0.003, 1.141e-06)
+# print(channel1.critical_depth())
+# print(channel1.normal_depth())
+# crit_depth = channel1.critical_depth()
+channel1.calculate()
+print(channel1.crit_depth)
+print(channel1.norm_depth)
+# print(channel1.chainage[0], channel1.energy[0], channel1.water[0], channel1.head[0])
+x = 0
+for i in channel1.chainage:
+    print("Length %.1f" % i, "Energy %.3f" % channel1.energy[x], "Water %.3f" % channel1.water[x], "Head %.3f" % channel1.head[x])
+    x += 1
+
+print("Test Run 2")
+channel1.setValues(0.5, 0.8, 2, 100.0, 1.0, 0.9, 0.003, 1.141e-06, 0.4, True, "DWCW", 0, 0)
+# print(channel1.critical_depth())
+# print(channel1.normal_depth())
+# crit_depth = channel1.critical_depth()
+channel1.calculate()
+print(channel1.crit_depth)
+print(channel1.norm_depth)
+# print(channel1.chainage[0], channel1.energy[0], channel1.water[0], channel1.head[0])
+x = 0
+for i in channel1.chainage:
+    print("Length %.1f" % i, "Energy %.3f" % channel1.energy[x], "Water %.3f" % channel1.water[x], "Head %.3f" % channel1.head[x])
+    x += 1
+
+print("Test Run 2 - manning")
+channel1.setValues(0.5, 0.8, 2, 100.0, 1.0, 0.9, 0.015, 1.141e-06, 0.4, True, "MANNING", 0, 0)
+# print(channel1.critical_depth())
+# print(channel1.normal_depth())
+# crit_depth = channel1.critical_depth()
+channel1.calculate()
+print(channel1.crit_depth)
+print(channel1.norm_depth)
+# print(channel1.chainage[0], channel1.energy[0], channel1.water[0], channel1.head[0])
+x = 0
+for i in channel1.chainage:
+    print("Length %.1f" % i, "Energy %.3f" % channel1.energy[x], "Water %.3f" % channel1.water[x], "Head %.3f" % channel1.head[x])
+    x += 1
---
+++ test_cases/pipe_test1.py
@@ -0,0 +1,35 @@
+import sys
+from os import path
+sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
+from pipe import Pipe
+# main
+"""enter flow, diameter, length, us invert, ds invert, Ks, kinematic viscocity"""
+# channel1 = channel.Channel(0.5, 1.0, 2.0, 100, 0.1, 0.0, 0.003, 1.141e-06, 0.4)
+print("Pipe Test Run 1")
+pipe1 = Pipe()
+pipe1.setValues(0.5, 1.0, 100.0, 1.0, 0.9, 0.003, 1.141e-06)
+# print(channel1.critical_depth())
+# print(channel1.normal_depth())
+# crit_depth = channel1.critical_depth()
+pipe1.calculate()
+print(pipe1.crit_depth)
+print(pipe1.norm_depth)
+# print(channel1.chainage[0], channel1.energy[0], channel1.water[0], channel1.head[0])
+x = 0
+for i in pipe1.chainage:
+    print("Length %.1f" % i, "Energy %.3f" % pipe1.energy[x], "Water %.3f" % pipe1.water[x], "Head %.3f" % pipe1.head[x])
+    x += 1
+
+print("Test Run 2")
+pipe1.setValues(0.5, 1.0, 100.0, 1.0, 0.9, 0.003, 1.141e-06, 0.4, False, "DWCW", 0, 0)
+# print(channel1.critical_depth())
+# print(channel1.normal_depth())
+# crit_depth = channel1.critical_depth()
+pipe1.calculate()
+print(pipe1.crit_depth)
+print(pipe1.norm_depth)
+# print(channel1.chainage[0], channel1.energy[0], channel1.water[0], channel1.head[0])
+x = 0
+for i in pipe1.chainage:
+    print("Length %.1f" % i, "Energy %.3f" % pipe1.energy[x], "Water %.3f" % pipe1.water[x], "Head %.3f" % pipe1.head[x])
+    x += 1
